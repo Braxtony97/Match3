07ccb292e10964757e07271fef9e15feaf44c871
Renat <painren@mail.ru>
2025-11-18 20:59:30 +0300

    Added game bootstrapper

---
+++ Assets/Match3/Scripts/Infrastructure/Game.cs
@@ -0,0 +1,9 @@
+﻿public class Game
+{
+    public readonly GameStateMachine StateMachine;
+
+    public Game(ICoroutineRunner coroutineRunner)
+    {
+        StateMachine = new GameStateMachine();
+    }
+}
---
+++ Assets/Match3/Scripts/Infrastructure/GameBootstrapper.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
+{
+    private Game _game;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        LoadGame();
+    }
+
+    private void LoadGame() => 
+        _game = new Game(this);
+}
